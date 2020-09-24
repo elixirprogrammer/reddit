3a31987b5f6f4b69827f2c0e7f901313fde4ade6
Anthony Gonzalez <boilercoding@gmail.com>
2020-09-24 01:46:10 -0400

    fixes post schema for new posts

--- lib/reddit/community/post.ex
+++ lib/reddit/community/post.ex
@@ -11,6 +11,7 @@ defmodule Reddit.Community.Post do
     field :body, :string
     field :title, :string
     field :votes_count, :integer, default: 0
+    field :comments_count, :integer, default: 0
     belongs_to :community, Community
     belongs_to :user, User
     has_many :comments, Comment
@@ -22,7 +23,7 @@ defmodule Reddit.Community.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :body, :upvotes, :downvotes, :comments_count])
+    |> cast(attrs, [:title, :body, :votes_count, :comments_count])
     |> validate_required([:title, :body])
   end
 end
