07fc65b16573fd1e6d5c7617359d01d7dd458450
Anthony Gonzalez <boilercoding@gmail.com>
2020-10-28 02:09:13 -0400

    adds communities list links sidebar

--- lib/reddit_web/controllers/page_controller.ex
+++ lib/reddit_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule RedditWeb.PageController do
   use RedditWeb, :controller
 
+  alias Reddit.Category
+
 ### def index(conn, _params) do
 ###    if Pow.Plug.current_user(conn) do
 ###     conn
@@ -11,6 +13,7 @@ defmodule RedditWeb.PageController do
 ###  end
 
   def index(conn, _params) do
-    render conn, :index
+    communities = Category.list_communities()
+    render conn, :index, communities: communities
   end
 end
