448d78915c6d58c7ccd56d1c88d7869d76ace3b6
Anthony Gonzalez <boilercoding@gmail.com>
2020-10-28 02:59:40 -0400

    fixes live votes when not logged in

--- lib/reddit_web/controllers/page_controller.ex
+++ lib/reddit_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule RedditWeb.PageController do
   use RedditWeb, :controller
 
   alias Reddit.Category
+  alias Reddit.Community, as: Post
 
 ### def index(conn, _params) do
 ###    if Pow.Plug.current_user(conn) do
@@ -14,6 +15,17 @@ defmodule RedditWeb.PageController do
 
   def index(conn, _params) do
     communities = Category.list_communities()
-    render conn, :index, communities: communities
+    posts = Post.list_posts()
+    logged_in_user_id = logged_in(conn.assigns.current_user)
+    render conn, :index, communities: communities, posts: posts, logged_in_user_id: logged_in_user_id
+  end
+
+  defp logged_in(current_user) do
+    case current_user do
+      nil ->
+        false
+      current_user ->
+        current_user.id
+    end
   end
 end
